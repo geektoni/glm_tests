f6ec07dba82ff73f8ae93ca95f86c4bc7eba5ca8
Giovanni De Toni <giovanni.det@gmail.com>
2019-04-30 17:16:41 +0200

    Add comments inside the test.py code.

--- test.py
+++ test.py
@@ -9,18 +9,24 @@ import scipy.sparse as sps
 #train_x = train_x.values
 #train_y = train_y.values.flatten()
 
-beta0 = pd.read_csv("negative_binomial_dataset/1000/beta0_1000_10.csv", header=None)
-beta = pd.read_csv("negative_binomial_dataset/1000/beta_1000_10.csv", header=None)
+# Read the betas
+beta0 = pd.read_csv("negative_binomial_dataset/10000/beta0_10000_10.csv", header=None)
+beta = pd.read_csv("negative_binomial_dataset/10000/beta_10000_10.csv", header=None)
 
+# Flatten the numpy array. The transformation will have this effect:
+# [[a], [b], [c]] ---> [a,b,c]
 beta0 = beta0.values.flatten()
 beta = beta.values.flatten()
 
-train_x = np.random.normal(0.0, 1.0, [1000, 10])
+# Generate random training data by using the previous betas
+train_x = np.random.normal(0.0, 1.0, [10000, 10])
 train_y = simulate_glm("neg-binomial", beta0, beta, train_x)
 
+# Create the GLM and train it
 glm = GLM(distr="neg-binomial")
 glm.fit(train_x, train_y)
 
+# Print the betas and the beta0 to check for correctness
 print("")
 print(glm.beta0_)
 print(glm.beta_)
